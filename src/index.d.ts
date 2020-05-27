4a48650e175e7f59f4b232ec70201c6c9d9e622a
ovanderzee <hastalavista@dds.nl>
2020-05-27 09:24:40 +0200

    simplyfied generated with tsc --declaration --allowJS --declarationDir src --emitDeclarationOnly src/index.js

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,6 @@
-declare module 'rgbLabConversion' {
-
-    interface rgbLabMethods {
-        rgb2XYZ: (rgbArray: number[], iccProfileName: string) => number[];
-        XYZ2Lab: (xyzArray: number[]) => number[];
-        rgb2Lab: (rgbArray: number[], iccProfileName: string) => number[];
-        cases: object;
-    }
-
-    export default function rgbLabConversion(): <rgbLabMethods>;
+declare namespace rgbLabConversion {
+    export function rgb2XYZ(rgbArray: number[], iccProfileName: string): number[];
+    export function XYZ2Lab(xyzArray: any): number[];
+    export function rgb2Lab(rgbArray: any, iccProfileName: any): number[];
 }
+export default rgbLabConversion;
